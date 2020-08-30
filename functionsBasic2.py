499476e1686a095caa8a3f97bdc9d81a9e47a342
dylanPMurphy <dylanmurphydba@gmail.com>
2020-08-29 23:08:49 -0700

    first_plud_length(list)

--- functionsBasic2.py
+++ functionsBasic2.py
@@ -19,7 +19,14 @@ print(print_and_return([2,4]))
 # First Plus Length - Create a function that accepts a list and returns the sum of the first value in the list plus the list's length.
 #     Example: first_plus_length([1,2,3,4,5]) should return 6 (first value: 1 + length: 5)
 
+def first_plus_length(list):
+    sum = 0
+    for i in range(0, len(list)):
+        sum+=list[i]
+        
+    print(f"first value: {list[0]} + length:{len(list)}")
 
+first_plus_length([3425,35,2,134,5,67,34,2,342,5,35,62,4457,45])
 # Values Greater than Second - Write a function that accepts a list and creates a new list containing only the values from the original list that are greater than its 2nd value. Print how many values this is and then return the new list. If the list has less than 2 elements, have the function return False
 #     Example: values_greater_than_second([5,2,3,2,1,4]) should print 3 and return [5,3,4]
 #     Example: values_greater_than_second([3]) should return False
