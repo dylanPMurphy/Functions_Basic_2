d5c371824b6a37747e8124ee4f2afd1891f7573f
dylanPMurphy <dylanmurphydba@gmail.com>
2020-08-29 22:44:57 -0700

    Countdown

--- functionsBasic2.py
+++ functionsBasic2.py
@@ -1,8 +1,13 @@
 
 # Countdown - Create a function that accepts a number as an input. Return a new list that counts down by one, from the number (as the 0th element) down to 0 (as the last element).
-#     Example: countdown(5) should return [5,4,3,2,1,0]
+#     Example: countdow(n(5) should return [5,4,3,2,1,0]
 
-
+def countdown(num):
+    output = []
+    for i in range(num,-1,-1):
+        output.append(i)
+    return output
+print(countdown(234))
 # Print and Return - Create a function that will receive a list with two numbers. Print the first value and return the second.
 #     Example: print_and_return([1,2]) should print 1 and return 2
 
